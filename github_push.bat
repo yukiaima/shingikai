55627378feff95bab8498e7cc7c4d12cf30e1830
yukiaima <k.isikawa@live.jp>
2023-11-26 21:58:29 +0900

    second commit

--- github_push.bat
+++ github_push.bat
@@ -1,4 +1,4 @@
 cd C:\Users\Koichiro_ISHIKAWA\マイドライブ（k.isikawa48016@gmail.com）\010_プライベート\040_勉強・情報収集\010_プログラミングでの自動化\010_審議会資料一覧取得
 git add .
 git commit -m '%DATE% %TIME% update'
-git push -f origin main
+git push -u origin main
