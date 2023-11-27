8422d4a5ac0615f764a509c4b4358f4d15311a76
yukiaima <k.isikawa@live.jp>
2023-11-28 08:30:14 +0900

    2023/11/28  8:30:14.42 update

--- github_push.bat
+++ github_push.bat
@@ -1,6 +1,9 @@
 @echo on
 cd C:\Users\Koichiro_ISHIKAWA\マイドライブ（k.isikawa48016@gmail.com）\010_プライベート\040_勉強・情報収集\010_プログラミングでの自動化\010_審議会資料一覧取得 && ^
 git add . && ^
+pause
 git commit -m "%DATE% %TIME% update" && ^
-git push origin main  && ^
+pause
+git push -u origin main  && ^
+pause
 exit /b 0
