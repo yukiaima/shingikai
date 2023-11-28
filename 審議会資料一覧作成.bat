aa17c2e5a392581b7931d60b4c32f59a0bdb86bf
yukiaima <k.isikawa@live.jp>
2023-11-29 06:45:32 +0900

    2023/11/29  6:45:31.76 update

--- 審議会資料一覧作成.bat
+++ 審議会資料一覧作成.bat
@@ -16,14 +16,12 @@ python 108_電力レジリエンス等に関する小委員会.py
 python 109_地内系統の混雑管理に関する勉強会.py
 python 110_運用容量検討会.py
 python 111_マージン検討会.py
-pause
 python 200_電力・ガス基本政策小委員会.py
 python 201_制度検討作業部会.py
 python 202_ガス事業制度検討ワーキンググループ.py
 python 203_再生可能エネルギー大量導入・次世代電力ネットワーク小委員会.py
 python 204_持続可能な電力システム構築小委員会.py
 python 205_系統ワーキンググループ.py
-pause
 python 206_あるべき卸電力市場、需給調整市場及び需給運用の実現に向けた実務検討作業部会.py
 python 207_卸電力市場、需給調整市場及び需給運用の在り方勉強会.py
 python 208_石油・天然ガス小委員会.py
@@ -33,15 +31,12 @@ python 211_電力広域的運営推進機関検証ワーキンググループ.py
 python 212_次世代の分散型電力システムに関する検討会.py 
 python 213_将来の電力需給に関する在り方勉強会.py
 python 214_同時市場の在り方等に関する検討会.py
-pause
 python 300_制度設計専門会合.py
 python 301_料金制度専門会合.py
-pause
 python 302_電気料金審査専門会合.py
 python 303_料金制度ワーキング・グループ.py
 python 304_送配電効率化・計画進捗確認ワーキンググループ.py
 python 900_博士人材の産業界への入職経路の多様化に関する勉強会.py
-pause
 
 rem 元のディレクトリへ移動
 cd ..
@@ -49,8 +44,6 @@ cd ..
 rem githubへpush
 git add . && ^
 git commit -m "%DATE% %TIME% update" && ^
-git push -u origin main  && ^
-
-pause
+git push -u origin main  
 
 exit /b 0
