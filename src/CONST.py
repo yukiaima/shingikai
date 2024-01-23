c415d730ea77d1461460e941cb144d1a64ebb2f2
yukiaima <k.isikawa@live.jp>
2024-01-23 19:30:33 +0900

    2024/01/23 19:30:33.26 update

--- src/CONST.py
+++ src/CONST.py
@@ -4,6 +4,6 @@ Created on Tue Jan 23 11:09:22 2024
 定数設定値
 @author: Koichiro_ISHIKAWA
 """
-CONNECT_TIMEOUT = 30 # html接続のタイムアウト
-READ_TIMEOUT = 30 # html読み込みのタイムアウト
+CONNECT_TIMEOUT = 10 # html接続のタイムアウト
+READ_TIMEOUT = 10 # html読み込みのタイムアウト
 
