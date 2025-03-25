a676125ae482c2d10754f0418d5a68786aa207c5
yukiaima <k.isikawa@live.jp>
2025-03-25 16:26:06 +0900

    2025/03/25 16:26:06.63 update

---
+++ src/CONST.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 11:09:22 2024
+定数設定値
+@author: Koichiro_ISHIKAWA
+"""
+CONNECT_TIMEOUT = 10 # html接続のタイムアウト
+READ_TIMEOUT = 10 # html読み込みのタイムアウト
+
