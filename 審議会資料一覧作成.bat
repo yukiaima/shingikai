8e8dcd01f4484bb0c8cfc93a94d2380b3be5bf2f
yukiaima <k.isikawa@live.jp>
2026-04-14 06:14:40 +0900

    2026/04/14  6:14:37.50 update

--- 審議会資料一覧作成.bat
+++ 審議会資料一覧作成.bat
@@ -3,11 +3,8 @@ setlocal
 rem pythonソースコードのディレクトリへ移動
 cd src
 
-rem pythonコードを実行 ※終わったものは更新しない
-python 200_METI審議会.py
-python 201_METI審議会（エネ庁）.py
-python 300_監視等委審議会.py
-python 400_内閣府審議会.py
+rem pythonコードを実行 
+python 010_審議会資料リンク集作成.py
 
 rem 元のディレクトリへ移動
 cd ..
